b798f8474513ff3ddcba32650b83dc9d59f80d3e
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-27 23:07:17 +0100

    "commit"

--- task/models.py
+++ task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TaskModel(models.Model):
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+
+def __str__(self):
+    return self.title
