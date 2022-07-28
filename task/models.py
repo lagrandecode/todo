945b7fb286e99bc671dd0cfe0145c4c56b5684f3
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-28 22:49:42 +0100

    "commit"

--- task/models.py
+++ task/models.py
@@ -7,7 +7,5 @@ class TaskModel(models.Model):
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
 
-
-
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
