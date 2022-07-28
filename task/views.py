52a037e34df935d7a931402bdb2f6418f13c38ef
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-28 22:40:10 +0100

    "commit"

--- task/views.py
+++ task/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import TaskModel
 
 # Create your views here.
 
 def task_view(request):
-    return render(request,"home.html")
+    modelobj = TaskModel.objects.all()
+    context ={
+        'modelobj' : modelobj
+
+    }
+    return render(request,"home.html", modelobj)
