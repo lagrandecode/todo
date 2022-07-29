2bba6edc6010b60bfea1a35becd85229612231b2
lagrandecode <seunogunmolufirst1@gmail.com>
2022-07-29 21:12:11 +0100

    "commit"

--- task/models.py
+++ task/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class TaskModel(models.Model):
     title = models.CharField(max_length=250)
+    desc = models.CharField(max_length=250, default='')
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
